b6b370e84711c4c394afb20950dbf95340adf117
Tomas <tomasarreguib@gmail.com>
2022-07-31 01:02:08 -0300

    added genres middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const auth = require("./auth-routes/index.js");
 const characters = require('./character-routes');
 const movies = require('./movie-routes');
+const genres = require('./genre-routes');
 
 const router = Router();
 
 router.use('/auth', auth)
 router.use('/characters', characters);
 router.use('/movies', movies)
+router.use('/genres', genres)
 
 module.exports = router;
