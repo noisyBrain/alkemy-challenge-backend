1b8da1d03cf6e32da1ac3b6c19fc506de13c7c74
Tomas <tomasarreguib@gmail.com>
2022-07-31 01:00:40 -0300

    added comments

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,15 +2,21 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../db');
 const { SECRET_TOKEN } = process.env;
 
+// funcion para validar el usuario
 const validateUser = async (req, res, next) => {
     try {
+        // extraemos el token del header
         const  token  = req.header("authToken");
-        console.log("token -> ", token)
+        // si no existe el token, no esta autorizado
         if (!token) return res.status(401).send("Unauthorized access")
+        // si existe, tomamos el token y lo verificamos
+        // contra el token secreto
         const payload = jwt.verify(token, SECRET_TOKEN)
+        // comprobamos que el usuario sea dueño del token provisto
         const user = await User.findByPk(payload.id)
+        // si no matchea, no hay usuario
         if (!user) return res.status(404).json({ msg: 'Not user found' })
-        // save user properties in the request
+        // si matchea, se pasa al siguiente middleware en la ruta
         next()
         
     } catch (error) {
