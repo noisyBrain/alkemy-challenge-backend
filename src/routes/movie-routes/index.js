2e06c790f3ee95cdf25fe86612b5a97a67a1cf01
Tomas <tomasarreguib@gmail.com>
2022-07-31 01:02:40 -0300

    add controllers to movie routes

--- src/routes/movie-routes/index.js
+++ src/routes/movie-routes/index.js
@@ -6,13 +6,20 @@ const putMovieController = require('../../controllers/movie-controllers/putMovie
 const deleteMovieController = require('../../controllers/movie-controllers/deleteMovie.controller');
 const sortMoviesByCreateController = require('../../controllers/movie-controllers/sortMoviesByCreate.controller.js');
 const getMovieByTitleController = require('../../controllers/movie-controllers/getMovieByTitle.controller.js');
+const filterMoviesByGenreController = require('../../controllers/movie-controllers/filterMoviesByGenre.controller.js');
 
 const router = require('express').Router()
 
-router.get('/:id', validateToken, getDetailMovieController);
-router.get('/', getAllMoviesController, getMovieByTitleController, sortMoviesByCreateController );
-router.post('/', validateToken, postMovieController);
-router.put('/:id', validateToken, putMovieController);
-router.delete('/:id', validateToken, deleteMovieController);
+// acordarse de agregar las validaciones en todas las rutas menos en el get All
+router.get('/:id', getDetailMovieController);
+router.get('/', 
+  getAllMoviesController, 
+  getMovieByTitleController, 
+  filterMoviesByGenreController, 
+  sortMoviesByCreateController 
+);
+router.post('/', postMovieController);
+router.put('/:id', putMovieController);
+router.delete('/:id', deleteMovieController);
 
 module.exports = router;
